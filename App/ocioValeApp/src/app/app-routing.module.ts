4cc4eb2073c32fa41665aff5dbfd474875be8cf7
Celia <celiabl@correo.ugr.es>
2019-10-30 10:51:56 +0100

    Comienzo de la interfaz de búsqueda
    Correccion del Header, Navegacion entre páginas Inicial y de búsqueda y comienzo de la interfaz de búsqueda

--- App/ocioValeApp/src/app/app-routing.module.ts
+++ App/ocioValeApp/src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  }
+  {path: '', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)},
+  { path: 'search', loadChildren: './search/search.module#SearchPageModule' }
 ];
 @NgModule({
   imports: [
--- App/ocioValeApp/src/app/home/home.page.ts
+++ App/ocioValeApp/src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderPage } from '../header/header.page';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-
-  constructor() {}
-
+  constructor(){
+    
+  }
 }
