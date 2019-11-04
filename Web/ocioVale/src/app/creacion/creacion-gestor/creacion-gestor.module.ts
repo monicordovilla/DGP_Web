3a7cb371e9f4b31cf02b57d0ae0fbc7795ffea12
monicordovilla <32360865+monicordovilla@users.noreply.github.com>
2019-11-04 13:24:48 +0100

    creado la carpeta de creacion y creacion-gestor

---
+++ Web/ocioVale/src/app/creacion/creacion-gestor/creacion-gestor.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './creacion-gestor.page';
+import { HeaderPage } from '../header/header.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [HomePage, HeaderPage]
+})
+export class HomePageModule {}
