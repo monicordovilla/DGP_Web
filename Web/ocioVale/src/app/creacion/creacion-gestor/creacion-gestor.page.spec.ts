64014752d4cd86bf30b41bb18ba4c5c015e6577a
monicordovilla <32360865+monicordovilla@users.noreply.github.com>
2019-11-10 20:54:27 +0100

    carpeta creacion arreglada

--- Web/ocioVale/src/app/creacion/creacion-gestor/creacion-gestor.page.spec.ts
+++ Web/ocioVale/src/app/creacion/creacion-gestor/creacion-gestor.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { CrearGestorPage } from './creacion-gestor.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('CrearGestorPage', () => {
+  let component: CrearGestorPage;
+  let fixture: ComponentFixture<CrearGestorPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ CrearGestorPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(CrearGestorPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
