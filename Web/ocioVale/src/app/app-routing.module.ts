c564892d1b2bc166c77619be30f1ded923e1faee
monicordovilla <32360865+monicordovilla@users.noreply.github.com>
2019-11-10 23:31:42 +0100

    problema del merge solucionado

--- Web/ocioVale/src/app/app-routing.module.ts
+++ Web/ocioVale/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'perfilGestor', loadChildren: () => import('./perfilGestor/perfilGestor.module').then( m=> m.perfilGestorPageModule)},
+  { path: 'creacion-gestor', loadChildren: () => import('./creacion/creacion-gestor/creacion-gestor.module').then( m=> m.CrearGestorPageModule)},
 ];
 
 @NgModule({
