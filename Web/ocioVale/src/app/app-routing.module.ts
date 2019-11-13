3d86fb82599e0e22ebc6ec1a2a3905d2d4367184
monicordovilla <32360865+monicordovilla@users.noreply.github.com>
2019-11-14 00:20:01 +0100

    arreglos del merge

--- Web/ocioVale/src/app/app-routing.module.ts
+++ Web/ocioVale/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'perfilGestor', loadChildren: () => import('./perfil/perfilGestor/perfilGestor.module').then( m=> m.perfilGestorPageModule)},
   { path: 'perfilVoluntario', loadChildren: () => import('./perfil/perfilVoluntario/perfilVoluntario.module').then( m=> m.perfilVoluntarioPageModule)},
-  { path: 'creacion-gestor', loadChildren: () => import('./creacion/creacion-gestor/creacion-gestor.module').then( m=> m.CrearGestorPageModule)},
+  { path: 'creacion', loadChildren: () => import('./creacion/creacion.module').then( m=> m.CrearPageModule)},
   { path: 'perfilSocioFamiliar', loadChildren: () => import('./perfil/perfilSocioFamiliar/perfilSocioFamiliar.module').then( m=> m.perfilSocioFamiliarPageModule)},
   { path: 'perfilSocio', loadChildren: () => import('./perfil/perfilSocio/perfilSocio.module').then( m=> m.perfilSocioPageModule)},
   { path: 'actividad', loadChildren: () => import('./perfil/actividad/actividad.module').then( m=> m.actividadPageModule)},
