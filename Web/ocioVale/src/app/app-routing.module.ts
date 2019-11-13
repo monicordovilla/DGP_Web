14d634d13e43ddca92e4fc65a5ea55e4f998bfea
AnaBosch <39536972+AnaBosch@users.noreply.github.com>
2019-11-14 00:10:50 +0100

    Update app-routing.module.ts

--- Web/ocioVale/src/app/app-routing.module.ts
+++ Web/ocioVale/src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ const routes: Routes = [
   { path: 'perfilGestor', loadChildren: () => import('./perfil/perfilGestor/perfilGestor.module').then( m=> m.perfilGestorPageModule)},
   { path: 'perfilVoluntario', loadChildren: () => import('./perfil/perfilVoluntario/perfilVoluntario.module').then( m=> m.perfilVoluntarioPageModule)},
   { path: 'creacion-gestor', loadChildren: () => import('./creacion/creacion-gestor/creacion-gestor.module').then( m=> m.CrearGestorPageModule)},
+  { path: 'perfilSocioFamiliar', loadChildren: () => import('./perfil/perfilSocioFamiliar/perfilSocioFamiliar.module').then( m=> m.perfilSocioFamiliarPageModule)},
+  { path: 'perfilSocio', loadChildren: () => import('./perfil/perfilSocio/perfilSocio.module').then( m=> m.perfilSocioPageModule)},
+  { path: 'actividad', loadChildren: () => import('./perfil/actividad/actividad.module').then( m=> m.actividadPageModule)},
   { path: 'usuarios', loadChildren: () => import('./usuarios/usuarios.module').then( m=> m.UsuariosModule)},
 ];
 
