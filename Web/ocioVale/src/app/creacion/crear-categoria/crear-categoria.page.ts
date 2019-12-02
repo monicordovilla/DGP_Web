cdc3c6ee9a65029115e4a65e8fe6c8352b5aa772
monicordovilla <32360865+monicordovilla@users.noreply.github.com>
2019-12-02 12:53:42 +0100

    form crear categoria

--- Web/ocioVale/src/app/creacion/crear-categoria/crear-categoria.page.ts
+++ Web/ocioVale/src/app/creacion/crear-categoria/crear-categoria.page.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCategoriaPage implements OnInit {
 
+  categorias = 
+    {
+      "nombre": '',
+      "imagen": ''
+    }
+
   constructor() { }
 
   ngOnInit() {
