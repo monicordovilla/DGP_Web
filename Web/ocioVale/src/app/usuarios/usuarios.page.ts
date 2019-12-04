9e4d397af50e21505d0332e1e88995b556624ec6
AnaBosch <39536972+AnaBosch@users.noreply.github.com>
2019-12-04 20:35:14 +0100

    Comienzo de la funcionalidad mostrar valoración

--- Web/ocioVale/src/app/usuarios/usuarios.page.ts
+++ Web/ocioVale/src/app/usuarios/usuarios.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'usuarios',
   templateUrl: 'usuarios.page.html',
   styleUrls: ['usuarios.page.scss'],
 })
-export class Usuarios {
+export class Usuarios implements OnInit {
+  private usuarios : FormGroup;
+
+  //Modificar
+  /*usuario = {
+    "nombre_usuario" : '',
+    "contraseña" : '',
+    "nombre" : '',
+    "apellidos" : '',
+    "mail" : '',
+    "fecha_nac" : '',
+    "telf" : '',
+    "localidad" : '',
+    "provincia" : '',
+    "descripcion" : ''
+  };*/
 
   constructor() {}
+
+  //Modificar
+  /*consultarPerfil(){
+    //Obtener de BD
+    //this.creacionActividad.value;
+  }*/
+
+  ngOnInit() {
+  }
 
 }
