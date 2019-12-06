0dca6ce79f5bef4bc477b4cb561bf0e2fe05c56b
nananoizy <morihuelacazorla@gmail.com>
2019-12-06 11:14:47 +0100

    proveedor aniadido

--- Web/ocioVale/src/app/actividades/actividades.page.ts
+++ Web/ocioVale/src/app/actividades/actividades.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ProveedorService} from '../providers/proveedor.service';
 
 @Component({
   selector: 'actividades',
@@ -7,6 +8,49 @@ import { Component } from '@angular/core';
 })
 export class Actividades {
 
-  constructor() {}
+  actividades=[];
+
+  categorias=[
+    [
+      {
+        "id": 0,
+        "imagen": "http://www.arasaac.org/repositorio/thumbs/10/200/3/30598.png"
+      },
+      {
+        "id": 0,
+        "imagen": "http://www.arasaac.org/repositorio/thumbs/10/200/2/24671.png"
+      }
+    ]
+  ];
+
+  constructor() {
+    this.cargaActividades();
+  }
+
+  cargaActividades(){
+    let dateTime;
+    let parts;
+
+    this.proveedor.obtenerActividades().subscribe(
+      (data) => {
+        this.actividades = data;
+
+        for(var i=0; i<this.actividades.length; i++){
+          dateTime = this.actividades[i].fecha;
+          parts= dateTime.split(/[- :TZ]/);
+          this.actividades[i].fecha = parts[2] + "-" + parts[1] + "-" + parts[0] + " | " + parts[3] + ":" + parts[4];
+          this.categorias.push([
+            {
+              "id": 1,
+              "imagen": "http://www.arasaac.org/repositorio/thumbs/10/200/3/30387.png"
+            }
+          ]);
+        }
+      },
+      error => {
+          console.log(<any>error);
+      }
+    )
+  }
 
 }
---
+++ Web/ocioVale/src/app/providers/proveedor.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable()
+export class ProveedorService {
+
+  constructor(public http:HttpClient) { }
+
+  obtenerActividades(): Observable<any>{
+    return this.http.get('http://localhost:3000/actividades');
+  }
+}
