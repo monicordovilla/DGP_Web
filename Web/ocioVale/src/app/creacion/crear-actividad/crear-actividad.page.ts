097aca3930b8168d16d4eb0f8f0027f448133e4c
AnaBosch <39536972+AnaBosch@users.noreply.github.com>
2019-12-15 00:08:17 +0100

    Alerta de la funcionalidad creación actividad

--- Web/ocioVale/src/app/creacion/crear-actividad/crear-actividad.page.ts
+++ Web/ocioVale/src/app/creacion/crear-actividad/crear-actividad.page.ts
@@ -36,8 +36,23 @@ export class CrearActividadPage implements OnInit {
     )
   }
 
+  mostrarMensaje(modal, type, message) {
+    let css = (type == "info" ? "message_box_info" : "message_box_error");
+    let messageBox = modal.querySelector('#messageBox');
+    messageBox.classList = "";
+    messageBox.classList.add(css);
+    messageBox.innerText = message;
+  }
+
   crearActividad(){
     console.log("crear actividad");
+
+    let modalLogin = document.getElementById('loginPopUp');
+    let form = modalLogin.querySelector("#formLogin");
+    modalLogin.style.display='block'
+
+    //let modal = document.getElementById('loginPopUp');
+    //modal.style.display='block'
 
     parseInt(localStorage.getItem("categoria.id"));
 
@@ -46,16 +61,23 @@ export class CrearActividadPage implements OnInit {
       this.maxsocios,
       this.maxvoluntarios
     ];
-
+    
     this.proveedor.enviarActividad(postData).subscribe(
       (res) => { 
+        modalLogin.style.backgroundColor= '#58E8C6';
+        this.mostrarMensaje(modalLogin, "info", "La actividad se ha creado correctamente");
         postData = res['results'];
+        
       },
       error =>{
+        modalLogin.style.backgroundColor= '#FF7C93';
+        this.mostrarMensaje(modalLogin, "info", "No se ha podido crear la actividad, intentelo de nuevo");
         console.error(error);
       }
     )
   }
+
+  
 
   ngOnInit() {
   }
